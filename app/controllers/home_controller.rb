bc0a7f870ed58133474a4de4a3718ce12049134d
kesleymartins <kesley.martins@vivaldi.net>
2022-08-06 21:10:23 -0300

    add events to the calendar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @events = Event.where(start_time: Time.now.beginning_of_month.beginning_of_week..Time.now.end_of_month.end_of_week)
   end
 end
