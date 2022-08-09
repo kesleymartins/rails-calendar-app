1e69e176c0bfb6e4db4c709c6a8ca62ff5d8af73
kesleymartins <kesley.martins@vivaldi.net>
2022-08-08 21:27:09 -0300

    remove tooltip controller to handle bootstrap tooltip

--- app/javascript/controllers/tooltip_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from '@hotwired/stimulus'
-
-export default class extends Controller {
-    connect() {
-        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-    }
-}
