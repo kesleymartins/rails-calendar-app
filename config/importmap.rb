47d37e01f66f4bf321e5314a456fea92130217f3
kesleymartins <kesley.martins@vivaldi.net>
2022-08-09 22:09:37 -0300

    install flatpickr

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 # Turbo
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+
+# Flatpicker for datetime
+pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
