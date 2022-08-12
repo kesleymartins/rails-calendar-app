1f83a743157018ffdfad099ec5a6be1bd2c0a797
kesleymartins <kesley.martins@vivaldi.net>
2022-08-12 01:27:24 -0300

    valites that start_time should be less then end_time

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ApplicationRecord
     validates :description, presence: true
     validates :start_time, presence: true
     validates :end_time, presence: true
+    validates :start_time, comparison: { less_than: :end_time }
 end
